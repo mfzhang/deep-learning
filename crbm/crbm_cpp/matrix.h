2da69c4e41e3f7f4f9182d031737f7a34e2cd0a1
chenrudan <chenrudan123@gmail.com>
2014-07-12 20:26:16 +0800

    matrix

---
+++ crbm/crbm_cpp/matrix.h
@@ -0,0 +1,10 @@
+/*************************************************************************
+    > File Name: matrix.h
+    > Author: chenrudan
+    > Mail: chenrudan123@gmail.com 
+    > Created Time: 2014年07月12日 星期六 20时25分15秒
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
